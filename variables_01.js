d771498282783f1091eb6ea7e0bd55ff4f7e6920
Gerardo Gutierrez <gerardo.gutierrez@nextiva.com>
2022-05-20 20:35:27 -0500

    CLASE 1 variables

---
+++ variables_01.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 2;
+let x = 1;
+let y = 2;
+
+function sumar(a, b) {
+  let resultado = a + b;
+  return resultado;
+}
+
+function mostrarResultado(valor) {
+  console.log(valor);
+}
+
+let resultado = sumar(x, y);
+
+//10
+mostrarResultado(resultado);
